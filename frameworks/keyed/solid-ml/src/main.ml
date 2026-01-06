ea8f5521a953c8a048f36fe36975e4d36f111fe7
sabine <6594573+sabine@users.noreply.github.com>
2026-01-06 05:31:22 +0100

    Rewrite solid-ml benchmark to use reactive primitives
    Now uses solid-ml's reactive features similar to SolidJS:
    - Signal for row labels (fine-grained reactivity)
    - Signal for selected row ID
    - Signal for data array
    - Batch for bulk updates
    - Effect for reactive DOM bindings (class, text content)
    - Keyed list rendering with DOM reconciliation
    
    Bundle size: 79kb (includes reactive runtime)

--- frameworks/keyed/solid-ml/src/main.ml
+++ frameworks/keyed/solid-ml/src/main.ml
@@ -1,6 +1,8 @@
 (** js-framework-benchmark implementation for solid-ml
     
-    This implements the standard benchmark operations:
+    This implements the standard benchmark operations using solid-ml's
+    reactive primitives (signals, effects, batch) similar to SolidJS.
+    
     - Create 1,000 rows
     - Create 10,000 rows  
     - Append 1,000 rows
@@ -37,290 +39,226 @@ let random max =
 
 let next_id = ref 1
 
-type row_data = {
+(** Row data with a signal for the label (like SolidJS) *)
+type row = {
   id : int;
-  mutable label : string;
+  label : string Reactive.Signal.t;
+  set_label : string -> unit;
 }
 
 let build_data count =
   Array.init count (fun _ ->
-    let label = 
+    let initial_label = 
       adjectives.(random (Array.length adjectives)) ^ " " ^
       colors.(random (Array.length colors)) ^ " " ^
       nouns.(random (Array.length nouns))
     in
+    let label, set_label = Reactive.Signal.create initial_label in
     let id = !next_id in
     incr next_id;
-    { id; label }
+    { id; label; set_label }
   )
-
-(** {1 DOM Helpers} *)
-
-(* Row template for cloning *)
-let row_template : Dom.element option ref = ref None
-
-let get_row_template () =
-  match !row_template with
-  | Some t -> t
-  | None ->
-    let tr = Dom.create_element Dom.document "tr" in
-    Dom.set_inner_html tr 
-      "<td class='col-md-1'></td><td class='col-md-4'><a></a></td><td class='col-md-1'><a><span class='glyphicon glyphicon-remove' aria-hidden='true'></span></a></td><td class='col-md-6'></td>";
-    row_template := Some tr;
-    tr
-
-(* Clone and populate a row *)
-let create_row (data : row_data) =
-  let template = get_row_template () in
-  let tr = Dom.clone_node template true in
-  (* Set data-id on tr for lookup *)
-  Dom.set_attribute tr "data-id" (string_of_int data.id);
-  (* Get child elements *)
-  let children = Dom.get_child_nodes tr in
-  (* TD 1: ID *)
-  let td1 = Dom.element_of_node children.(0) in
-  Dom.node_set_text_content (Dom.node_of_element td1) (string_of_int data.id);
-  (* TD 2: Label *)
-  let td2 = Dom.element_of_node children.(1) in
-  let a = Dom.get_first_child td2 in
-  (match a with
-   | Some a_node -> Dom.node_set_text_content a_node data.label
-   | None -> ());
-  tr
 
 (** {1 Application State} *)
 
-let data : row_data array ref = ref [||]
-let rows : Dom.element array ref = ref [||]
-let selected_row : Dom.element option ref = ref None
-let selected_id : int option ref = ref None
+(* Main data signal - array of rows *)
+let data, set_data = Reactive.Signal.create [||]
 
-let get_tbody () =
-  match Dom.get_element_by_id Dom.document "tbody" with
-  | Some el -> el
-  | None -> failwith "tbody not found"
-
-let get_table () =
-  match Dom.query_selector Dom.document "table" with
-  | Some el -> el
-  | None -> failwith "table not found"
-
-(** Find row index by id *)
-let find_idx id =
-  let d = !data in
-  let len = Array.length d in
-  let rec loop i =
-    if i >= len then None
-    else if d.(i).id = id then Some i
-    else loop (i + 1)
-  in
-  loop 0
-
-(** Get parent row ID from click target *)
-let get_parent_id (target : Dom.element) : int option =
-  let rec find el =
-    let tag = Dom.get_tag_name el in
-    if tag = "TR" then
-      match Dom.get_attribute el "data-id" with
-      | Some id_str -> int_of_string_opt id_str
-      | None -> None
-    else
-      match Dom.get_parent_element el with
-      | Some parent -> find parent
-      | None -> None
-  in
-  find target
-
-(** Unselect current row *)
-let unselect () =
-  (match !selected_row with
-   | Some row -> Dom.set_class_name row ""
-   | None -> ());
-  selected_row := None;
-  selected_id := None
-
-(** Select row by index *)
-let select idx =
-  unselect ();
-  let r = !rows in
-  if idx >= 0 && idx < Array.length r then begin
-    let row = r.(idx) in
-    Dom.set_class_name row "danger";
-    selected_row := Some row;
-    selected_id := Some (!data).(idx).id
-  end
-
-(** Delete row by index *)
-let delete idx =
-  let d = !data in
-  let r = !rows in
-  if idx >= 0 && idx < Array.length d then begin
-    (* Check if we're deleting the selected row *)
-    let deleting_selected = !selected_id = Some d.(idx).id in
-    (* Remove from DOM *)
-    let row = r.(idx) in
-    (match Dom.get_parent_element row with
-     | Some parent -> Dom.remove_child parent (Dom.node_of_element row)
-     | None -> ());
-    (* Remove from arrays *)
-    data := Array.concat [Array.sub d 0 idx; Array.sub d (idx + 1) (Array.length d - idx - 1)];
-    rows := Array.concat [Array.sub r 0 idx; Array.sub r (idx + 1) (Array.length r - idx - 1)];
-    (* Clear selection if we deleted the selected row *)
-    if deleting_selected then unselect ()
-  end
-
-(** Remove all rows *)
-let remove_all_rows () =
-  let tbody = get_tbody () in
-  Dom.node_set_text_content (Dom.node_of_element tbody) ""
-
-(** Append rows to tbody *)
-let append_rows new_data =
-  let tbody = get_tbody () in
-  let table = get_table () in
-  let new_rows = Array.map create_row new_data in
-  (* Optimization: remove tbody, add rows, re-add tbody *)
-  let was_empty = Array.length !rows = 0 in
-  if was_empty then
-    Dom.remove_child table (Dom.node_of_element tbody);
-  Array.iter (fun row ->
-    Dom.append_child tbody (Dom.node_of_element row)
-  ) new_rows;
-  if was_empty then
-    Dom.append_child table (Dom.node_of_element tbody);
-  (* Update state *)
-  data := Array.append !data new_data;
-  rows := Array.append !rows new_rows
+(* Selected row ID signal *)
+let selected, set_selected = Reactive.Signal.create (-1)
 
 (** {1 Benchmark Operations} *)
 
 let run () =
-  remove_all_rows ();
-  data := [||];
-  rows := [||];
-  unselect ();
-  append_rows (build_data 1000)
+  set_data (build_data 1000)
 
 let run_lots () =
-  remove_all_rows ();
-  data := [||];
-  rows := [||];
-  unselect ();
-  append_rows (build_data 10000)
+  set_data (build_data 10000)
 
 let add () =
-  append_rows (build_data 1000)
+  let current = Reactive.Signal.peek data in
+  set_data (Array.append current (build_data 1000))
 
-let update () =
-  let d = !data in
-  let r = !rows in
-  let len = Array.length d in
-  let i = ref 0 in
-  while !i < len do
-    (* Update data *)
-    d.(!i).label <- d.(!i).label ^ " !!!";
-    (* Update DOM *)
-    let row = r.(!i) in
-    let children = Dom.get_child_nodes row in
-    let td2 = Dom.element_of_node children.(1) in
-    (match Dom.get_first_child td2 with
-     | Some a_node -> Dom.node_set_text_content a_node d.(!i).label
-     | None -> ());
-    i := !i + 10
-  done
+let update_rows () =
+  Reactive.Batch.run (fun () ->
+    let d = Reactive.Signal.peek data in
+    let len = Array.length d in
+    let i = ref 0 in
+    while !i < len do
+      let row = d.(!i) in
+      row.set_label (Reactive.Signal.peek row.label ^ " !!!");
+      i := !i + 10
+    done
+  )
 
 let clear () =
-  remove_all_rows ();
-  data := [||];
-  rows := [||];
-  unselect ()
+  set_data [||]
 
 let swap_rows () =
-  let d = !data in
-  let r = !rows in
+  let d = Reactive.Signal.peek data in
   if Array.length d > 998 then begin
-    (* Swap in data *)
-    let tmp_d = d.(1) in
-    d.(1) <- d.(998);
-    d.(998) <- tmp_d;
-    (* Swap in rows array *)
-    let tmp_r = r.(1) in
-    r.(1) <- r.(998);
-    r.(998) <- tmp_r;
-    (* Swap in DOM: move row at index 1 before row at index 2 *)
-    let tbody = get_tbody () in
-    Dom.insert_before tbody (Dom.node_of_element r.(1)) (Some (Dom.node_of_element r.(2)));
-    (* Move row at index 998 before its next sibling (or append if last) *)
-    Dom.insert_before tbody (Dom.node_of_element r.(998)) (Dom.get_next_sibling r.(998))
+    let new_data = Array.copy d in
+    let tmp = new_data.(1) in
+    new_data.(1) <- new_data.(998);
+    new_data.(998) <- tmp;
+    set_data new_data
   end
 
-(** {1 Event Handling} *)
+let remove id =
+  let d = Reactive.Signal.peek data in
+  set_data (Array.of_list (
+    Array.to_list d |> List.filter (fun row -> row.id <> id)
+  ))
 
-let handle_button_click (target : Dom.element) =
-  let id = Dom.get_id target in
-  match id with
-  | "run" -> run ()
-  | "runlots" -> run_lots ()
-  | "add" -> add ()
-  | "update" -> update ()
-  | "clear" -> clear ()
-  | "swaprows" -> swap_rows ()
-  | _ -> ()
+let select id =
+  set_selected id
 
-let handle_tbody_click target =
-  (* Find which TD was clicked *)
-  let rec find_td el =
-    let tag = Dom.get_tag_name el in
-    if tag = "TD" then Some el
-    else match Dom.get_parent_element el with
-      | Some parent -> find_td parent
-      | None -> None
-  in
-  match find_td target with
-  | None -> ()
-  | Some td ->
-    match Dom.get_parent_element td with
-    | None -> ()
-    | Some tr ->
-      let children = Dom.get_child_nodes tr in
-      (* Check if click was on label (td index 1) or delete (td index 2) *)
-      let is_label_td = Array.length children > 1 && Dom.element_of_node children.(1) = td in
-      let is_delete_td = Array.length children > 2 && Dom.element_of_node children.(2) = td in
-      match get_parent_id target with
-      | None -> ()
-      | Some id ->
-        if is_label_td then begin
-          match find_idx id with
-          | Some idx -> select idx
-          | None -> ()
-        end
-        else if is_delete_td then begin
-          match find_idx id with
-          | Some idx -> delete idx
-          | None -> ()
-        end
+(** {1 Row Rendering} *)
 
-(** {1 Initialization} *)
+(** Render a single row. The label is reactive via signal. *)
+let render_row row =
+  let row_id = row.id in
+  let tr = Dom.create_element Dom.document "tr" in
+  
+  (* Reactive class binding for selection *)
+  Reactive.Effect.create (fun () ->
+    let sel = Reactive.Signal.get selected in
+    Dom.set_class_name tr (if sel = row_id then "danger" else "")
+  );
+  
+  (* TD 1: ID *)
+  let td1 = Dom.create_element Dom.document "td" in
+  Dom.set_attribute td1 "class" "col-md-1";
+  Dom.node_set_text_content (Dom.node_of_element td1) (string_of_int row_id);
+  Dom.append_child tr (Dom.node_of_element td1);
+  
+  (* TD 2: Label with reactive text *)
+  let td2 = Dom.create_element Dom.document "td" in
+  Dom.set_attribute td2 "class" "col-md-4";
+  let a = Dom.create_element Dom.document "a" in
+  Dom.add_event_listener a "click" (fun _ -> select row_id);
+  (* Reactive text content *)
+  let text_node = Dom.create_text_node Dom.document (Reactive.Signal.peek row.label) in
+  Reactive.Effect.create (fun () ->
+    Dom.text_set_data text_node (Reactive.Signal.get row.label)
+  );
+  Dom.append_child a (Dom.node_of_text text_node);
+  Dom.append_child td2 (Dom.node_of_element a);
+  Dom.append_child tr (Dom.node_of_element td2);
+  
+  (* TD 3: Delete button *)
+  let td3 = Dom.create_element Dom.document "td" in
+  Dom.set_attribute td3 "class" "col-md-1";
+  let a_del = Dom.create_element Dom.document "a" in
+  Dom.add_event_listener a_del "click" (fun _ -> remove row_id);
+  let span = Dom.create_element Dom.document "span" in
+  Dom.set_attribute span "class" "glyphicon glyphicon-remove";
+  Dom.set_attribute span "aria-hidden" "true";
+  Dom.append_child a_del (Dom.node_of_element span);
+  Dom.append_child td3 (Dom.node_of_element a_del);
+  Dom.append_child tr (Dom.node_of_element td3);
+  
+  (* TD 4: Spacer *)
+  let td4 = Dom.create_element Dom.document "td" in
+  Dom.set_attribute td4 "class" "col-md-6";
+  Dom.append_child tr (Dom.node_of_element td4);
+  
+  tr
+
+(** {1 Keyed List Rendering} *)
+
+(** Efficient keyed list rendering with minimal DOM updates.
+    Similar to SolidJS's <For> component. *)
+let render_keyed_list ~(items : row array Reactive.Signal.t) (parent : Dom.element) =
+  (* Map from row id to DOM element *)
+  let node_map : (int, Dom.element) Hashtbl.t = Hashtbl.create 1024 in
+  let current_ids : int array ref = ref [||] in
+  
+  Reactive.Effect.create (fun () ->
+    let new_items = Reactive.Signal.get items in
+    let new_ids = Array.map (fun r -> r.id) new_items in
+    let old_ids = !current_ids in
+    
+    (* Build set of new IDs for O(1) lookup *)
+    let new_id_set = Hashtbl.create (Array.length new_ids) in
+    Array.iter (fun id -> Hashtbl.replace new_id_set id ()) new_ids;
+    
+    (* Remove nodes that are no longer in the list *)
+    Array.iter (fun old_id ->
+      if not (Hashtbl.mem new_id_set old_id) then begin
+        match Hashtbl.find_opt node_map old_id with
+        | Some node -> 
+          Dom.remove_child parent (Dom.node_of_element node);
+          Hashtbl.remove node_map old_id
+        | None -> ()
+      end
+    ) old_ids;
+    
+    (* Get or create nodes for all items *)
+    let nodes = Array.map (fun row ->
+      match Hashtbl.find_opt node_map row.id with
+      | Some node -> node
+      | None ->
+        let node = render_row row in
+        Hashtbl.replace node_map row.id node;
+        node
+    ) new_items in
+    
+    (* Reconcile DOM order - use insertBefore for efficiency *)
+    let len = Array.length nodes in
+    for i = 0 to len - 1 do
+      let node = nodes.(i) in
+      let current_child = 
+        if i < Array.length old_ids then
+          Hashtbl.find_opt node_map old_ids.(i)
+        else
+          None
+      in
+      (* Insert node at correct position if not already there *)
+      match current_child with
+      | Some curr when curr == node -> () (* Already in place *)
+      | _ ->
+        let next_sibling = 
+          if i + 1 < len then Some (Dom.node_of_element nodes.(i + 1))
+          else None
+        in
+        Dom.insert_before parent (Dom.node_of_element node) next_sibling
+    done;
+    
+    current_ids := new_ids
+  );
+  
+  (* Cleanup *)
+  Reactive.Owner.on_cleanup (fun () ->
+    Hashtbl.iter (fun _ node ->
+      Dom.remove_child parent (Dom.node_of_element node)
+    ) node_map;
+    Hashtbl.clear node_map
+  )
+
+(** {1 Main App} *)
 
 let () =
   Random.self_init ();
   
-  (* Set up button click handlers via event delegation *)
-  (match Dom.get_element_by_id Dom.document "main" with
-   | Some main ->
-     Dom.add_event_listener main "click" (fun evt ->
-       let target = Dom.target evt in
-       let tag = Dom.get_tag_name target in
-       if tag = "BUTTON" then
-         handle_button_click target
-     )
-   | None -> ());
-  
-  (* Set up tbody click handlers via event delegation *)
-  (match Dom.get_element_by_id Dom.document "tbody" with
-   | Some tbody ->
-     Dom.add_event_listener tbody "click" (fun evt ->
-       let target = Dom.target evt in
-       handle_tbody_click target
-     )
-   | None -> ())
+  match Dom.get_element_by_id Dom.document "tbody" with
+  | None -> Dom.error "tbody not found"
+  | Some tbody ->
+    (* Set up keyed list rendering *)
+    let _dispose = Reactive.Owner.create_root (fun () ->
+      render_keyed_list ~items:data tbody
+    ) in
+    
+    (* Button handlers *)
+    let setup_button id handler =
+      match Dom.get_element_by_id Dom.document id with
+      | Some btn -> Dom.add_event_listener btn "click" (fun _ -> handler ())
+      | None -> ()
+    in
+    
+    setup_button "run" run;
+    setup_button "runlots" run_lots;
+    setup_button "add" add;
+    setup_button "update" update_rows;
+    setup_button "clear" clear;
+    setup_button "swaprows" swap_rows
